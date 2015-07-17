c0012b8e7cfb4ec5b8520b6a64b9e71dd6928eb0
Ben Anding <benanding@MacBook.local>
2015-07-17 09:22:57 -0400

    test

--- Platforms/iOS/Documentation/search/all_5.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['licensepath',['licensePath',['../interface_a_f_d_x_detector.html#aa524901e7e089a0e7f5ec0261f544265',1,'AFDXDetector']]],
-  ['lipcornerdepressor',['lipCornerDepressor',['../interface_a_f_d_x_detector.html#a6f9faec22e79eef9273874030127032c',1,'AFDXDetector']]]
-];
--- Platforms/iOS/Documentation/search/properties_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['percentfacedetected',['percentFaceDetected',['../interface_a_f_d_x_detector.html#a5701eb6ba1ac76188d4e4ab9a566b851',1,'AFDXDetector']]]
-];
