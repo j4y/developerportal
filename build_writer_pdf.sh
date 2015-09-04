4bdfcf2f24c74071fb6375a1f5bd224c852afb71
Ben Anding <benanding@ip-192-168-1-9.ec2.internal>
2015-09-03 21:15:31 -0400

    mismatching urls

--- build_writer_pdf.sh
+++ build_writer_pdf.sh
@@ -1,6 +1,6 @@
 jekyll serve --config_base.yml,config_writer.yml,config config_writer_pdf.yml&
 sleep 9
 
-prince --input-list=../documentation-theme-jekyll-builds/writer-pdf/prince-file-list.txt -o ../documentation-theme-jekyll-builds/pdf-output/writer.pdf
+prince --input-list=../developerportal/writer-pdf/prince-file-list.txt -o ../developerportal/pdf-output/writer.pdf
 
 kill -9 $(ps aux | grep '[j]ekyll' | awk '{print $2}')
