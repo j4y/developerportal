b3503afac1c8c5ad07d9dad6f544f82996065f31
Ben Anding <benjamin.anding@affectiva.com>
2016-01-25 17:07:26 -0500

    removed extraneous files

--- build_designer_pdf.sh
+++
@@ -1,6 +0,0 @@
-jekyll serve --config config_base.yml,config_designer.yml,config_designer_pdf.yml&
-sleep 9
-
-prince --input-list=../developerportal/designer-pdf/prince-file-list.txt -o ../developerportal/pdf-output/designer.pdf
-
-kill -9 $(ps aux | grep '[j]ekyll' | awk '{print $2}')
